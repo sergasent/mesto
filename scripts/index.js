e845d7d1c3df64a8f339ceddf0f58848811fddd2
Sergey Berdnikov <sega12b@yandex.ru>
2023-02-06 14:07:11 +0500

    feat: add JS implementation of the card-list initial load

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,35 @@
+const initialCards = [
+  {
+    name: "Байкал",
+    link: "./images/cards/564/baikal.jpg"
+  },
+  {
+    name: "Алтай, Чемал",
+    link: "./images/cards/564/chemal,altai.jpg"
+  },
+  {
+    name: "Эльбрус",
+    link: "./images/cards/564/elbrus.jpg"
+  },
+  {
+    name: "Камчатка",
+    link: "./images/cards/564/kamchatka.jpg"
+  },
+  {
+    name: "Великий Новгород",
+    link: "./images/cards/564/velikiy-novgorod.jpg"
+  },
+  {
+    name: "Волгоград",
+    link: "./images/cards/564/volgograd.jpg"
+  },
+];
+
+const cardsList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('.cards__template').content;
+const cardAddButton = document.querySelector('.profile__add-button');
+
+
 const editProfileBtn = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
@@ -8,6 +40,25 @@ const popupProfileForm = document.querySelector('.profile-form');
 const popupProfileName = document.querySelector('.profile-form__input_type_username');
 const popupProfileDescription = document.querySelector('.profile-form__input_type_description');
 const popupCloseBtn = document.querySelector('.popup__close-button');
+
+
+function addCards(...cards) {
+  cards.forEach(item => {
+    const card = cardTemplate.cloneNode(true);
+
+    const cardImage = card.querySelector('.card__image');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+
+    card.querySelector('.card__title').textContent = item.name;
+
+    cardsList.prepend(card);
+  });
+}
+
+function fillInitialCards() {
+  addCards(...initialCards);
+}
 
 
 function openProfilePopup() {
@@ -29,6 +80,10 @@ function handleFormSubmit(evt) {
 
   closePopup();
 }
+
+document.addEventListener('DOMContentLoaded', fillInitialCards);
+
+//cardAddButton.addEventListener('click', fillInitialCards);
 
 
 editProfileBtn.addEventListener('click', openProfilePopup);
